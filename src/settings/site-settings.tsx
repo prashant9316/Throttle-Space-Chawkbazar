68195be45e7a9852accfc61d936749da54e0c818
prashant9316 <pgmd.9873@gmail.com>
2024-02-05 04:57:32 +0530

    updated the mobile menu

--- src/settings/site-settings.tsx
+++ src/settings/site-settings.tsx
@@ -241,342 +241,204 @@ export const siteSettings = {
     ],
     mobileMenu: [
       {
-        id: 2,
-        path: '/search?q=men-wear',
-        label: 'menu-men-wear',
-        subMenu: [
-          {
-            id: 1,
-            path: '/search?q=top-wear',
-            label: 'menu-top-wear',
-            subMenu: [
-              {
-                id: 1,
-                path: '/search?q=t-shit-shirtrt',
-                label: 'menu-t-shirt',
-              },
-              {
-                id: 2,
-                path: '/search?q=casual-shirts',
-                label: 'menu-casual-shirts',
-              },
-              {
-                id: 3,
-                path: '/search?q=formal-shirts',
-                label: 'menu-formal-shirts',
-              },
-              {
-                id: 4,
-                path: '/search?q=blazwers-coats',
-                label: 'menu-blazwers-coats',
-              },
-              {
-                id: 5,
-                path: '/search?q=suits',
-                label: 'menu-suits',
-              },
-              {
-                id: 6,
-                path: '/search?q=jackets',
-                label: 'menu-jackets',
-              },
-            ],
-          },
-          {
-            id: 2,
-            path: '/search?q=belt-scarves',
-            label: 'menu-belt-scarves',
-          },
-          {
-            id: 3,
-            path: '/search?q=watches-wearables',
-            label: 'menu-watches-wearables',
-          },
-          {
-            id: 4,
-            path: '/search?q=western-wear',
-            label: 'menu-western-wear',
-            subMenu: [
-              {
-                id: 1,
-                path: '/search?q=dresses',
-                label: 'menu-dresses',
-              },
-              {
-                id: 2,
-                path: '/search?q=jumpsuits',
-                label: 'menu-jumpsuits',
-              },
-              {
-                id: 3,
-                path: '/search?q=tops-t-shirt',
-                label: 'menu-tops-shirts',
-              },
-              {
-                id: 4,
-                path: '/search?q=shorts-skirts',
-                label: 'menu-shorts-skirts',
-              },
-              {
-                id: 5,
-                path: '/search?q=shurgs',
-                label: 'menu-shurgs',
-              },
-              {
-                id: 6,
-                path: '/search?q=blazers',
-                label: 'menu-blazers',
-              },
-            ],
-          },
-          {
-            id: 5,
-            path: '/search?q=plus-size',
-            label: 'menu-plus-size',
-          },
-          {
-            id: 6,
-            path: '/search?q=sunglasses-frames',
-            label: 'menu-sunglasses-frames',
-          },
-          {
-            id: 7,
-            path: '/search?q=footwear',
-            label: 'menu-footwear',
-            subMenu: [
-              {
-                id: 1,
-                path: '/search?q=flats',
-                label: 'menu-flats',
-              },
-              {
-                id: 2,
-                path: '/search?q=casual-shoes',
-                label: 'menu-casual-shoes',
-              },
-              {
-                id: 3,
-                path: '/search?q=heels',
-                label: 'menu-heels',
-              },
-              {
-                id: 4,
-                path: '/search?q=boots',
-                label: 'menu-boots',
-              },
-            ],
-          },
-          {
-            id: 8,
-            path: '/search?q=sports-active-wear',
-            label: 'menu-sports-active-wear',
-            subMenu: [
-              {
-                id: 1,
-                path: '/search?q=clothing',
-                label: 'menu-clothing',
-              },
-              {
-                id: 2,
-                path: '/search?q=footwear',
-                label: 'menu-footwear',
-              },
-              {
-                id: 3,
-                path: '/search?q=sports-accessories',
-                label: 'menu-sports-accessories',
-              },
-            ],
-          },
-          {
-            id: 9,
-            path: '/search?q=lingerie-sleepwear',
-            label: 'menu-lingerie-sleepwear',
-            subMenu: [
-              {
-                id: 1,
-                path: '/search?q=bra',
-                label: 'menu-bra',
-              },
-              {
-                id: 2,
-                path: '/search?q=briefs',
-                label: 'menu-briefs',
-              },
-              {
-                id: 3,
-                path: '/search?q=sleepwear',
-                label: 'menu-sleepwear',
-              },
-            ],
-          },
-          {
-            id: 10,
-            path: '/search?q=belt-scarves',
-            label: 'menu-belt-scarves',
-            subMenu: [
-              {
-                id: 1,
-                path: '/search?q=makeup',
-                label: 'menu-makeup',
-              },
-              {
-                id: 2,
-                path: '/search?q=skincare',
-                label: 'menu-skincare',
-              },
-              {
-                id: 3,
-                path: '/search?q=premium-beauty',
-                label: 'menu-premium-beauty',
-              },
-              {
-                id: 4,
-                path: '/search?q=lipsticks',
-                label: 'menu-lipsticks',
-              },
-            ],
-          },
-          {
-            id: 11,
-            path: '/search?q=gadgets',
-            label: 'menu-gadgets',
-            subMenu: [
-              {
-                id: 1,
-                path: '/search?q=smart-wearables',
-                label: 'menu-smart-wearables',
-              },
-              {
-                id: 2,
-                path: '/search?q=headphones',
-                label: 'menu-headphones',
-              },
-            ],
-          },
-          {
-            id: 12,
-            path: '/search?q=jewellers',
-            label: 'menu-jewellers',
-            subMenu: [
-              {
-                id: 1,
-                path: '/search?q=fashion-jewellers',
-                label: 'menu-fashion-jewellers',
-              },
-              {
-                id: 2,
-                path: '/search?q=fine-jewellers',
-                label: 'menu-fine-jewellers',
-              },
-            ],
-          },
-          {
-            id: 13,
-            path: '/search?q=backpacks',
-            label: 'menu-backpacks',
-          },
-          {
-            id: 14,
-            path: '/search?q=handbags-wallets',
-            label: 'menu-handbags-wallets',
-          },
-        ],
-      },
-      {
-        id: 3,
-        path: '/search?q=women-wear',
-        label: 'menu-women-wear',
-      },
-      {
-        id: 4,
+        id: 1,
         path: '/search',
-        label: 'menu-search',
-      },
-      {
-        id: 5,
-        path: '/shops',
-        label: 'menu-shops',
-      },
-      {
-        id: 6,
-        path: '/',
-        label: 'menu-pages',
+        label: 'Frames',
         subMenu: [
           {
             id: 1,
             path: '/',
-            label: 'menu-users',
-            subMenu: [
-              {
-                id: 1,
-                path: '/my-account',
-                label: 'menu-my-account',
-              },
-              {
-                id: 2,
-                path: '/signin',
-                label: 'menu-sign-in',
-              },
-              {
-                id: 3,
-                path: '/signup',
-                label: 'menu-sign-up',
-              },
-              {
-                id: 4,
-                path: '/forget-password',
-                label: 'menu-forget-password',
-              },
-            ],
+            label: '1-2 Inch Frames',
           },
           {
             id: 2,
-            path: '/faq',
-            label: 'menu-faq',
+            path: '/',
+            label: '3-4 Inch Frames',
           },
           {
             id: 3,
-            path: '/privacy',
-            label: 'menu-privacy-policy',
+            path: '/',
+            label: '5-6 Inch Frames',
           },
           {
             id: 4,
-            path: '/terms',
-            label: 'menu-terms-condition',
-          },
-          {
-            id: 5,
-            path: '/contact-us',
-            label: 'menu-contact-us',
-          },
-          {
-            id: 6,
-            path: '/checkout',
-            label: 'menu-checkout',
-          },
-          {
-            id: 7,
-            path: '/collections/mens-collection',
-            label: 'menu-collection',
-          },
-          {
-            id: 8,
-            path: '/category/man',
-            label: 'menu-category',
-          },
-          {
-            id: 9,
-            path: '/order',
-            label: 'menu-order',
-          },
-          {
-            id: 10,
-            path: '/404',
-            label: 'menu-404',
+            path: '/',
+            label: '7-10 Inch Frames',
           },
         ],
       },
+      {
+        id: 2,
+        path: '/search?q=women-wear',
+        label: 'Electronics',
+        subMenu: [
+          {
+            id: 1,
+            label: 'Flight Controllers',
+            path: '/',
+          },
+          {
+            id: 2,
+            label: 'Electronic Speed Controllers',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: 'Stacks',
+            path: '/',
+          },
+          {
+            id: 4,
+            label: 'AIOs',
+            path: '/',
+          },
+        ]
+      },
+      {
+        id: 3,
+        path: '/',
+        label: 'Radios',
+      },
+      {
+        id: 4,
+        path: '/',
+        label: 'FPV Equipment',
+        subMenu: [
+          {
+            id: 1,
+            label: 'FPV Goggles',
+            path: '/',
+          },
+          {
+            id: 2,
+            label: 'FPV Cameras',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: 'VTX',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: 'VRX',
+            path: '/',
+          },
+        ],
+      },
+      {
+        id: 5,
+        path: '/search?q=accessories',
+        label: 'Motors',
+        subMenu: [
+          {
+            id: 1,
+            label: 'Micro Motors',
+            path: '/',
+          },
+          {
+            id: 2,
+            label: 'Mini Motors',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: 'Long Range',
+            path: '/',
+          },
+        ],
+      },
+      {
+        id: 6,
+        path: '/search?q=accessories',
+        label: 'Batteries',
+        subMenu: [
+          {
+            id: 1,
+            label: '1S Batteries',
+            path: '/',
+          },
+          {
+            id: 2,
+            label: '2S Batteries',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: '3S Batteries',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: '4S Batteries',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: '6S Batteries',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: 'Li-Ion Batteries',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: 'Custom Packs',
+            path: '/',
+          },
+        ],
+      },
+      {
+        id: 7,
+        path: '/',
+        label: 'RTF & PNP',
+        subMenu: [
+          {
+            id: 1,
+            label: 'Ready to Fly Builds',
+            path: '/',
+          },
+          {
+            id: 2,
+            label: 'Customize your build',
+            path: '/',
+          },
+          {
+            id: 3,
+            label: 'ThrottleSpace Exclusives',
+            path: '/',
+          },
+        ],
+      },
+      {
+        id: 7,
+        label: "Tools & Accessories",
+        path: "/",
+        subMenu: [
+          {
+            id: 1,
+            label: 'Multimeters',
+            path: '/',
+          },
+          {
+            id: 1,
+            label: 'Tools',
+            path: '/',
+          },
+          {
+            id: 1,
+            label: 'Soldering Irons',
+            path: '/',
+          },
+          {
+            id: 1,
+            label: 'Heat Shrink Tubes',
+          },
+        ],
+      },
+
     ],
     languageMenu: [
       {
